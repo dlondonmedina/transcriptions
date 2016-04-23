cb1ea9e4cc06a151a3939deb41e3c032b1b5a8f2
dlondonmedina <medinad@uw.edu>
2016-04-23 12:44:05 -0700

    continue setup

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ except
 config = {
 	'description': 'Transcription Project',
 	'author': 'Dylan Medina',
-	'url': 'URL to get it',
-	'download_url': 'Where to download it.',
-	'author_email': 'medinad@uw.edu',
+	'url': 'https://github.com/dlondonmedina/transcription',
+	'download_url': 'https://github.com/dlondonmedina/transcription',
+	'author_email': 'dylan@dylanmedina.com',
 	'version': '0.1',
-	'install_requires': ['none'],
+	'install_requires': ['speech_transcription', 'pocketsphinx', 'sphinxbase'],
 	'packages': ['transcription'],
 	'scripts': [],
 	'name': 'transcriptionproject'
 }
 
 setup(**config)
-
